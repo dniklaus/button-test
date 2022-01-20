88df6831564653514a65c780e647e7e04a019725
Dieter Niklaus <dieter.niklaus@betterask.erni>
2022-01-20 21:08:39 +0100

    get rid of unused includes

--- lib/prod-dbg/ProductDebug.cpp
+++ lib/prod-dbg/ProductDebug.cpp
@@ -8,16 +8,6 @@
 #include "ProductDebug.h"
 
 #include <Arduino.h>
-#include <Timer.h>
-#include <SerialCommand.h>
-#include <DbgCliNode.h>
-#include <DbgCliTopic.h>
-#include <DbgCliCommand.h>
-#include <DbgTraceContext.h>
-#include <DbgTracePort.h>
-#include <DbgTraceLevel.h>
-#include <DbgPrintConsole.h>
-#include <DbgTraceOut.h>
 #include <AppDebug.h>
 
 
