e96267e7a241e0363b9640aebe0408bba6380e79
Dieter Niklaus <dieter.niklaus@betterask.erni>
2022-01-20 21:13:46 +0100

    Bump Timer to SpinTimer 3.0.0

--- src/button-test.cpp
+++ src/button-test.cpp
@@ -5,20 +5,12 @@
  *      Author: niklausd
  */
 
-
-#define WIRINGTIMER_SUPPRESS_WARNINGS
-#include <Timer.h>          // pio lib install 1699, lib details see https://github.com/dniklaus/wiring-timer
+#include <SpinTimer.h>      // pio lib install 11599, lib details see https://github.com/dniklaus/spin-timer
 #include <SerialCommand.h>  // pio lib install 173, lib details see https://github.com/kroimon/Arduino-SerialCommand
+#include <DbgTracePort.h>
+#include <DbgTraceLevel.h>
 
 // private libraries
-#include <DbgCliNode.h>
-#include <DbgCliTopic.h>
-#include <DbgCliCommand.h>
-#include <DbgTracePort.h>
-#include <DbgTraceContext.h>
-#include <DbgTraceOut.h>
-#include <DbgPrintConsole.h>
-#include <DbgTraceLevel.h>
 #include <AppDebug.h>
 #include <ProductDebug.h>
 #include <RamUtils.h>
